4e3986e96f2a4118dcdb5b8fd0a7b2f50d75aabb
Kinsasha <kennedyokechukwu203@gmail.com>
2025-04-04 11:19:50 +0100

    Completed tasks 6 & 7 on TOP's Foundations: Javascript loops and arrays tasks

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isDivisibleByFour = year % 4 === 0;
+    const isDivisibleByHundred = year % 100 === 0;
+    const isDivisibleByFourHundred = year % 400 === 0;
 
+    if (year = isDivisibleByFour && (!isDivisibleByHundred || isDivisibleByFourHundred)) {
+        return true
+    } else {
+        return false
+    }
 };
+console.log(leapYears(1600));
 
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(toCelsius) {
+  let conversion = (toCelsius - 32) * 5/9;
+  return Math.round(conversion * 10)/10; 
 };
 
-const convertToFahrenheit = function() {
+console.log(convertToCelsius(32));
+
+const convertToFahrenheit = function(toFahrenheit) {
+  let convert = (toFahrenheit * 9/5 + 32);
+  return Math.round(convert * 10)/10;
 };
+console.log(convertToFahrenheit(0));
+
 
 // Do not edit below this line
 module.exports = {
